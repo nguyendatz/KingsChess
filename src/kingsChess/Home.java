93c8a2e41f04bf8d61f0d87cc3e7c0170f18a661
nguyendatz <nquocdat94@gmail.com>
2017-05-21 23:05:55 +0700

    Init project

---
+++ src/kingsChess/Home.java
@@ -0,0 +1,17 @@
+package kingsChess;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class Home extends JPanel {
+	
+	Home()
+	{}
+	
+	public void paint(Graphics g)
+	{
+		
+	}
+
+}
